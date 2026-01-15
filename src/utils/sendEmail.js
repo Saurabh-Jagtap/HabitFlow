2de99078c787422182775b6eed91fbc875efbd5c
Shruti J <Jagtapshruti46@gmail.com>
2026-01-15 17:35:50 +0530

    fix: update sendEmail function to ensure proper port conversion and improve email sender format

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,16 +4,20 @@ export const sendEmail = async ({ to, subject, html }) => {
 
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false, // true only for port 465
+    port: Number(process.env.SMTP_PORT),
+    secure: false, 
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
+    tls: {
+        ciphers: "SSLv3",
+    },
+    family: 4, // Forces IPv4
   });
 
   await transporter.sendMail({
-    from: process.env.FROM_EMAIL,
+    from: `"HabitFlow Support" <${process.env.SMTP_USER}>`, 
     to,
     subject,
     html,
